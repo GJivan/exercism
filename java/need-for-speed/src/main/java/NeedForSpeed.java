04ab81ba1a004c99afe8e4a1efae28f5881f7afd
Gopal <gopal.jivan@gmail.com>
2023-06-06 13:40:11 -0700

    Adds Initial Files

---
+++ java/need-for-speed/src/main/java/NeedForSpeed.java
@@ -0,0 +1,27 @@
+class NeedForSpeed {
+    // TODO: define the constructor for the 'NeedForSpeed' class
+
+    public boolean batteryDrained() {
+        throw new UnsupportedOperationException("Please implement the NeedForSpeed.batteryDrained() method");
+    }
+
+    public int distanceDriven() {
+        throw new UnsupportedOperationException("Please implement the NeedForSpeed.distanceDriven() method");
+    }
+
+    public void drive() {
+        throw new UnsupportedOperationException("Please implement the NeedForSpeed.drive() method");
+    }
+
+    public static NeedForSpeed nitro() {
+        throw new UnsupportedOperationException("Please implement the (static) NeedForSpeed.nitro() method");
+    }
+}
+
+class RaceTrack {
+    // TODO: define the constructor for the 'RaceTrack' class
+
+    public boolean tryFinishTrack(NeedForSpeed car) {
+        throw new UnsupportedOperationException("Please implement the RaceTrack.tryFinishTrack() method");
+    }
+}
