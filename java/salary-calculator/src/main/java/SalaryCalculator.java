c76356472fff5069361953eecdf026eda202dfc8
Gopal <gopal.jivan@gmail.com>
2023-06-11 13:54:43 -0700

    Adds second solution

--- java/salary-calculator/src/main/java/SalaryCalculator.java
+++ java/salary-calculator/src/main/java/SalaryCalculator.java
@@ -1,6 +1,7 @@
 public class SalaryCalculator {
 
     private static final double capSalary = 2000.00;
+    private static final double baseSalary = 1000.00;
 
     public double multiplierPerDaysSkipped(int daysSkipped) {
         return daysSkipped > 5 ? .85 : 1;
@@ -15,8 +16,8 @@ public class SalaryCalculator {
     }
 
     public double finalSalary(int daysSkipped, int productsSold) {
-        double baseSalary = (1000.000 * multiplierPerDaysSkipped(daysSkipped))
+        double salary = (baseSalary * multiplierPerDaysSkipped(daysSkipped))
                 + bonusForProductSold(productsSold);
-        return Math.min(baseSalary, capSalary);
+        return Math.min(salary, capSalary);
     } 
 }
