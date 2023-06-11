8e3955dc2f4d430b26945594e5d929cbeba8c68f
Gopal <gopal.jivan@gmail.com>
2023-06-11 12:39:51 -0700

    Adds solution and implementation

--- java/need-for-speed/src/main/java/NeedForSpeed.java
+++ java/need-for-speed/src/main/java/NeedForSpeed.java
@@ -1,27 +1,51 @@
 class NeedForSpeed {
-    // TODO: define the constructor for the 'NeedForSpeed' class
 
-    public boolean batteryDrained() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.batteryDrained() method");
-    }
+    private final int speed;
+    private final int batteryDrain;
+    private int drivenDistance = 0;
+    private int battery = 100;
 
-    public int distanceDriven() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.distanceDriven() method");
-    }
-
-    public void drive() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.drive() method");
+    public NeedForSpeed(int speed, int batteryDrain) {
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
     }
 
     public static NeedForSpeed nitro() {
-        throw new UnsupportedOperationException("Please implement the (static) NeedForSpeed.nitro() method");
+        return new NeedForSpeed(50, 4);
+    }
+
+    public boolean batteryDrained() {
+        return battery == 0;
+    }
+
+    public int distanceDriven() {
+        return drivenDistance;
+    }
+
+    public void drive() {
+        if (!batteryDrained()) {
+            drivenDistance += speed;
+            battery -= batteryDrain;
+        }
+
     }
 }
 
 class RaceTrack {
     // TODO: define the constructor for the 'RaceTrack' class
 
+    private final int trackLength;
+
+    public RaceTrack(int trackLength) {
+        this.trackLength = trackLength;
+    }
+
     public boolean tryFinishTrack(NeedForSpeed car) {
-        throw new UnsupportedOperationException("Please implement the RaceTrack.tryFinishTrack() method");
+        while (!car.batteryDrained()) {
+            car.drive();
+        }
+
+        return car.distanceDriven() >= trackLength;
+
     }
 }
