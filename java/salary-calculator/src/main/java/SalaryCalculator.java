329f7083063ca78ab4c7eff7e865cc00fc725fad
Gopal <gopal.jivan@gmail.com>
2023-06-11 13:50:18 -0700

    Adds first solution

--- java/salary-calculator/src/main/java/SalaryCalculator.java
+++ java/salary-calculator/src/main/java/SalaryCalculator.java
@@ -1,17 +1,22 @@
 public class SalaryCalculator {
+
+    private static final double capSalary = 2000.00;
+
     public double multiplierPerDaysSkipped(int daysSkipped) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.multiplierPerDaysSkipped() method");
+        return daysSkipped > 5 ? .85 : 1;
     }
 
     public int multiplierPerProductsSold(int productsSold) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.multiplierPerProductsSold() method");
+        return productsSold > 20 ? 13 : 10;
     }
 
     public double bonusForProductSold(int productsSold) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.bonusForProductSold() method");
+        return productsSold * multiplierPerProductsSold(productsSold);
     }
 
     public double finalSalary(int daysSkipped, int productsSold) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.finalSalary() method");
+        double baseSalary = (1000.000 * multiplierPerDaysSkipped(daysSkipped))
+                + bonusForProductSold(productsSold);
+        return Math.min(baseSalary, capSalary);
     } 
 }
