43153db2ed3dc51da5e15d52b8b35ea16896e9e8
Gopal <gopal.jivan@gmail.com>
2023-07-03 13:26:07 -0700

    Adds 1st Solution

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,11 +1,54 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Scrabble {
 
+    private final String word;
+
+    private static final Map<Character, Integer> LETTER_SCORES = new HashMap<>();
+
+    static {
+        // Initialize letter scores
+        LETTER_SCORES.put('A', 1);
+        LETTER_SCORES.put('B', 3);
+        LETTER_SCORES.put('C', 3);
+        LETTER_SCORES.put('D', 2);
+        LETTER_SCORES.put('E', 1);
+        LETTER_SCORES.put('F', 4);
+        LETTER_SCORES.put('G', 2);
+        LETTER_SCORES.put('H', 4);
+        LETTER_SCORES.put('I', 1);
+        LETTER_SCORES.put('J', 8);
+        LETTER_SCORES.put('K', 5);
+        LETTER_SCORES.put('L', 1);
+        LETTER_SCORES.put('M', 3);
+        LETTER_SCORES.put('N', 1);
+        LETTER_SCORES.put('O', 1);
+        LETTER_SCORES.put('P', 3);
+        LETTER_SCORES.put('Q', 10);
+        LETTER_SCORES.put('R', 1);
+        LETTER_SCORES.put('S', 1);
+        LETTER_SCORES.put('T', 1);
+        LETTER_SCORES.put('U', 1);
+        LETTER_SCORES.put('V', 4);
+        LETTER_SCORES.put('W', 4);
+        LETTER_SCORES.put('X', 8);
+        LETTER_SCORES.put('Y', 4);
+        LETTER_SCORES.put('Z', 10);
+    }
+
     Scrabble(String word) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.word = word;
     }
 
     int getScore() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int score = 0;
+        for (char c : word.toUpperCase().toCharArray()) {
+            if (LETTER_SCORES.containsKey(c)) {
+                score += LETTER_SCORES.get(c);
+            }
+        }
+        return score;
     }
 
 }
