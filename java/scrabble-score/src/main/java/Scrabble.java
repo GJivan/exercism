a03b1c6ea7029f7cec7e15bd36f82bd6199cb93d
Gopal <gopal.jivan@gmail.com>
2023-07-03 13:29:32 -0700

    Adds 2nd Solution

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -5,50 +5,25 @@ class Scrabble {
 
     private final String word;
 
-    private static final Map<Character, Integer> LETTER_SCORES = new HashMap<>();
+    private static final Map<Integer, Integer> lScore = new HashMap<>(26);
 
     static {
         // Initialize letter scores
-        LETTER_SCORES.put('A', 1);
-        LETTER_SCORES.put('B', 3);
-        LETTER_SCORES.put('C', 3);
-        LETTER_SCORES.put('D', 2);
-        LETTER_SCORES.put('E', 1);
-        LETTER_SCORES.put('F', 4);
-        LETTER_SCORES.put('G', 2);
-        LETTER_SCORES.put('H', 4);
-        LETTER_SCORES.put('I', 1);
-        LETTER_SCORES.put('J', 8);
-        LETTER_SCORES.put('K', 5);
-        LETTER_SCORES.put('L', 1);
-        LETTER_SCORES.put('M', 3);
-        LETTER_SCORES.put('N', 1);
-        LETTER_SCORES.put('O', 1);
-        LETTER_SCORES.put('P', 3);
-        LETTER_SCORES.put('Q', 10);
-        LETTER_SCORES.put('R', 1);
-        LETTER_SCORES.put('S', 1);
-        LETTER_SCORES.put('T', 1);
-        LETTER_SCORES.put('U', 1);
-        LETTER_SCORES.put('V', 4);
-        LETTER_SCORES.put('W', 4);
-        LETTER_SCORES.put('X', 8);
-        LETTER_SCORES.put('Y', 4);
-        LETTER_SCORES.put('Z', 10);
+        "AEIOULNRST".chars().forEach(c -> lScore.put(c, 1));
+        "DG".chars().forEach(c -> lScore.put(c, 2));
+        "BCMP".chars().forEach(c -> lScore.put(c, 3));
+        "FHVWY".chars().forEach(c -> lScore.put(c, 4));
+        "K".chars().forEach(c -> lScore.put(c, 5));
+        "JX".chars().forEach(c -> lScore.put(c, 8));
+        "QZ".chars().forEach(c -> lScore.put(c, 10));
     }
 
     Scrabble(String word) {
-        this.word = word;
+        this.word = word.toUpperCase();
     }
 
     int getScore() {
-        int score = 0;
-        for (char c : word.toUpperCase().toCharArray()) {
-            if (LETTER_SCORES.containsKey(c)) {
-                score += LETTER_SCORES.get(c);
-            }
-        }
-        return score;
+        return word.chars().reduce(0, (a, b) -> a + lScore.get(b));
     }
 
 }
