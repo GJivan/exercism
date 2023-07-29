94820447369c0fdad549cf081b0fa25eaeb0d7d0
Gopal <gopal.jivan@gmail.com>
2023-07-29 08:22:41 -0700

    Fixes checkstyle

--- java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,16 +1,14 @@
 class DifferenceOfSquaresCalculator {
-
     int computeSquareOfSumTo(int input) {
         int sum = (input * (input + 1)) / 2;
         return sum * sum;
     }
 
     int computeSumOfSquaresTo(int input) {
-        return ((input * (input + 1 ) * (2*input+1))/6 );
+        return ((input * (input + 1) * (2 * input + 1)) / 6);
     }
 
     int computeDifferenceOfSquares(int input) {
         return (computeSquareOfSumTo(input) - computeSumOfSquaresTo(input));
     }
-
 }
