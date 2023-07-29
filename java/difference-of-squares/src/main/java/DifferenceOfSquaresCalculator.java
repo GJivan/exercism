288ad339d0529eaab91e7414620fa695fd36cd64
Gopal <gopal.jivan@gmail.com>
2023-07-29 08:18:12 -0700

    First Pass

--- java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,15 +1,16 @@
 class DifferenceOfSquaresCalculator {
 
     int computeSquareOfSumTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int sum = (input * (input + 1)) / 2;
+        return sum * sum;
     }
 
     int computeSumOfSquaresTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return ((input * (input + 1 ) * (2*input+1))/6 );
     }
 
     int computeDifferenceOfSquares(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return (computeSquareOfSumTo(input) - computeSumOfSquaresTo(input));
     }
 
 }
