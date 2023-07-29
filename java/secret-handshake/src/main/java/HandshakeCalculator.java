b50ced322bcd9c5f015f9b639058667b175da644
Gopal <gopal.jivan@gmail.com>
2023-07-29 08:55:27 -0700

    Adds second solution

--- java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,32 +1,31 @@
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 class HandshakeCalculator {
 
     List<Signal> calculateHandshake(int number) {
-        String binary = Integer.toBinaryString(number);
         List<Signal> signals = new ArrayList<>();
 
-        if ("1".equals(binary.substring(binary.length()-1)))
+        if ((number & 1) == 1) {
             signals.add(Signal.WINK);
+        }
 
-        if (binary.length() >= 2
-                && "1".equals(binary.substring(binary.length()-2,binary.length()-1)))
+        if ((number & 2) == 2) {
             signals.add(Signal.DOUBLE_BLINK);
+        }
 
-        if (binary.length() >= 3
-                && "1".equals(binary.substring(binary.length()-3,binary.length()-2)))
+        if ((number & 4) == 4) {
             signals.add(Signal.CLOSE_YOUR_EYES);
+        }
 
-        if (binary.length() >= 4
-                && "1".equals(binary.substring(binary.length()-4,binary.length()-3)))
+        if ((number & 8) == 8) {
             signals.add(Signal.JUMP);
+        }
 
-        if (binary.length() >= 5
-                && "1".equals(binary.substring(binary.length()-5,binary.length()-4)))
+        if ((number & 16) == 16) {
             Collections.reverse(signals);
+        }
 
 
         return signals;
