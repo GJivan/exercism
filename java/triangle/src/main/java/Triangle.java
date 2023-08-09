35ecd01571efbd62fb982329194537c3c7d94f18
Gopal <gopal.jivan@gmail.com>
2023-08-09 14:12:31 -0700

    Adds Soultion #2

--- java/triangle/src/main/java/Triangle.java
+++ java/triangle/src/main/java/Triangle.java
@@ -1,39 +1,41 @@
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+import static java.util.Arrays.asList;
+
 class Triangle {
 
-    private static double side1;
-    private static double side2;
-    private static double side3;
+    List<Double> triangle;
 
     Triangle(double side1, double side2, double side3) throws TriangleException {
 
-        if (side1 <= 0 || side2 <= 0 || side3 <= 0 || !isTriangleValid(side1,  side2,  side3)) {
-            throw new TriangleException();
-        }
-
-        this.side1 = side1;
-        this.side2 = side2;
-        this.side3 = side3;
+        triangle = Optional.of(asList(side1, side2, side3))
+                .filter( legalTriangle )
+                .orElseThrow(() -> new TriangleException());
     }
 
     boolean isEquilateral() {
-        return side1 == side2 && side2 == side3;
+        return triangle.stream().distinct().count() == 1;
 
     }
 
     boolean isIsosceles() {
-        return side1 == side2 || side2 == side3 || side1 == side3;
+        return triangle.stream().distinct().count() < 3;
+
     }
 
     boolean isScalene() {
-        return side1 != side2 && side2 != side3;
+        return triangle.stream().distinct().count() == 3;
     }
 
-    private boolean isTriangleValid(double a, double b, double c) {
-        if((a+b)>c && (a+c)>b && (b+c)>a) {
-            return true;
-        }else {
-            return false;
-        }
-    }
+    private static Predicate<List<Double>> legalTriangle = l -> {
+        double sum = l.stream()
+                .mapToDouble(Double::doubleValue)
+                .sum();
+        return l.stream()
+                .allMatch(side ->
+                        (sum - side > side) && (side > 0));
+    };
 
 }
