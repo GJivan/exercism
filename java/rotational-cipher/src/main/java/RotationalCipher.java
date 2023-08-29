255c31b32e3144e31d509520cb49b9e2cb8729ff
Gopal <gopal.jivan@gmail.com>
2023-08-28 21:20:46 -0700

    First Soultion

--- java/rotational-cipher/src/main/java/RotationalCipher.java
+++ java/rotational-cipher/src/main/java/RotationalCipher.java
@@ -1,11 +1,29 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 class RotationalCipher {
 
+    int shiftKey;
     RotationalCipher(int shiftKey) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.shiftKey = shiftKey;
     }
 
     String rotate(String data) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        StringBuilder result = new StringBuilder();
+        for (char character : data.toCharArray()) {
+            if (character != ' ' && Character.isLetter(character)) {
+                int originalAlphabetPosition = Character.isUpperCase(character) ? character - 'A' : character - 'a';
+                int newAlphabetPosition = (originalAlphabetPosition + shiftKey) % 26;
+                char newCharacter = (char)  (Character.isUpperCase(character) ? ('A' + newAlphabetPosition) : ('a' + newAlphabetPosition) )   ;
+                result.append(newCharacter);
+            } else {
+                result.append(character);
+            }
+        }
+        return result.toString();
+
     }
 
 }
